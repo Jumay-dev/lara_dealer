b461805a85d5272bf50c4dd6c05d8a2e696daaa9
Jumaydev <osipovm33rus@gmail.com>
2020-12-28 17:04:53 +0300

    добавлен create и get для ЛУ, тест для клиники по проекту успешен

--- app/Http/Controllers/ClinicalController.php
+++ app/Http/Controllers/ClinicalController.php
@@ -9,18 +9,69 @@ class ClinicalController extends Controller
     protected $table = 'clinics';
 
     public function create() {
-        
+        $clinic = new \App\Models\Clinic;
+        $clinic->external_id = request('external_id');
+        $clinic->brand = request('brand');
+        $clinic->ur_name = request('ur_name');
+        $clinic->inn = request('inn');
+        $clinic->region = request('region');
+        $clinic->city = request('city');
+        $clinic->street = request('street');
+        $clinic->house = request('house');
+        $clinic->block = request('block');
+        $clinic->office = request('office');
+        $clinic->comment = request('comment');
+        try {
+            $clinic->saveOrFail();
+            if ($clinic->id) {
+                return response()->json([
+                    'success' => true,
+                    'result' => "ЛУ создано"
+                ]);
+            } else {
+                return response()->json([
+                    'success' => true,
+                    'result' => "Ошибка создания ЛУ"
+                ]);
+            }
+
+        }
+        catch(\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'error' => $e
+            ]);
+        }
     }
 
     public function get() {
+        $id = request('id');
 
+        if(isset($id) && $id !== '') {
+            $clinic = new \App\Models\Clinic;
+            $proj_res = $clinic->find($id);
+            if ($proj_res) {
+                return response()->json([
+                    'success' => true,
+                    'result' => $proj_res,
+                ]);
+            }
+            return response()->json([
+                'success' => false,
+                'result' => 'ЛУ не найдена'
+            ]);
+        }
+        return response()->json([
+            'success' => false,
+            'result' => "Не указан ID ЛУ"
+        ]);
     }
 
-    public function edit() {
-        
+    public function update() {
+
     }
 
     public function delete() {
-        
+
     }
 }
--- app/Models/Clinic.php
+++ app/Models/Clinic.php
@@ -7,4 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class Clinic extends ExtraModel
 {
     protected $table = 'clinics';
+
+    public static function setObject(){
+        return "project";
+    }
+
+    public function saveOrFail(array $options = [])
+    {
+        if (!$this->created_by) {
+            $this->created_by = auth()->user()->id;
+        }
+        if (!$this->updated_by) {
+            $this->updated_by = auth()->user()->id;
+        }
+        return parent::saveOrFail($options);
+    }
 }
